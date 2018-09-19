ef3b4df10798b2ad13428df056879b7487c73b18
MMB2 <git@mmb2.click>
2018-09-19 16:53:59 +0200

    eval

--- evaluator.py
+++ evaluator.py
@@ -1,24 +1,51 @@
 from naiveUtil import getMeanGlobalRatingSlow
-from getData import getRatingSets
-
+from getData import getRatingSets, rmse
+from sklearn import metrics
+import numpy as np
+import time
+import json
 
 sets = getRatingSets()
 
 
-# take set
+toTest = [
+  {
+    "func": getMeanGlobalRatingSlow,
+    "name": "Global Average",
+    "rmses": [],
+    "maes": [],
+    "ratings": [],
+    "runtime": []
+  }
+]
+
 rmseForMeanRating = []
+maeForMeanRating = []
+print("going through subsets")
 for subSet in sets:
-  meanRating = []
-  # for each test entry, estimate rating using training set
-  for testData in subSet["test"]:
-    meanRating.append(getMeanGlobalRatingSlow(subSet["train"]))
-  
-  # get ratings
-  # compare error
-  meanRatingError = rmse(subSet.test, meanRating)
-  print("mean error is", meanRatingError)
-  rmseForMeanRating.append(meanRatingError)
+  for algo in toTest:
+    start = time.time()
+    algo["ratings"] = []
+    meanRating = []
+    # for each test entry, estimate rating using training set
+    i = 0
+    for testData in subSet["test"]:
+      print("testset calc ", i)
+      i += 1
+      algo["ratings"].append(algo["func"](subSet["train"], testData["movieId"], testData["userId"]))
+    # get ratings
+    # compare error
+    meanRatingError = rmse(np.array(subSet["testNumOnly"]), np.array(algo["ratings"]))
+    absoluteRatingError = metrics.mean_absolute_error(np.array(subSet["testNumOnly"]), np.array(algo["ratings"]))
+    algo["rmses"].append(meanRatingError)
+    algo["maes"].append(absoluteRatingError)
+    algo["runtime"].append(time.time() - start)
   # save error
 
-print("the mean rating rmses are", rmseForMeanRating, np.mean(rmseForMeanRating))
+print("rmses are", toTest[0]["rmses"], np.mean(toTest[0]["rmses"]), np.mean(toTest[0]["runtime"]))
 # do the same for MAE
+
+for algo in toTest:
+  algo["ratings"] = None
+  algo["func"] = None
+print(json.dumps(toTest))
--- naiveUtil.py
+++ naiveUtil.py
@@ -1,28 +1,43 @@
 from getData import getRatings, allNumericRatings, getRatingsForMovie, getRatingsForUser, getRatingSets
 import numpy as np
 
-def getMeanGlobalRatingSlow(ratings=getRatings()):
-  total = 0.0
-  amount = 0.0
-  for rating in ratings:
-    total += rating["rating"]
-    amount += 1
-  return total / amount
+calculatedMeans = {}
+calculatedItemMeans = {}
+
+def getMeanGlobalRatingSlow(ratings, movieId, userId):
+  key = str(ratings[0]["userId"]) + str(ratings[0]["movieId"])
+  if calculatedMeans.get(key) is None:
+    total = 0.0
+    amount = 0.0
+    for rating in ratings:
+      total += rating["rating"]
+      amount += 1
+    mean = total / amount
+    calculatedMeans[key] = mean
+    return mean
+  else:
+    return calculatedMeans[key]
 
 def getMeanGlobalRatingFast():
   ratings = np.array(allNumericRatings())
   return np.mean(ratings)
 
-def getMeanRatingForItem(movieId, ratings=getRatings()):
-  movieRatings = getRatingsForMovie(movieId, ratings)
-  total = 0.0
-  amount = 0.0
-  for rating in movieRatings:
-    total += rating["rating"]
-    amount += 1
-  return total / amount
+def getMeanRatingForItem(ratings, movieId, userId):
+  key = str(movieId) + str(ratings[0]["userId"]) + str(ratings[0]["movieId"])
+  if calculatedItemMeans.get(key) is None:
+    movieRatings = getRatingsForMovie(movieId, ratings)
+    total = 0.0
+    amount = 0.0
+    for rating in movieRatings:
+      total += rating["rating"]
+      amount += 1
+    itemMean = total / amount
+    calculatedItemMeans[key] = itemMean
+    return itemMean
+  else:
+    return calculatedItemMeans[key]
 
-def getMeanRatingForUser(userId, ratings=getRatings()):
+def getMeanRatingForUser(ratings, movieId, userId):
   movieRatings = getRatingsForUser(userId, ratings)
   total = 0.0
   amount = 0.0
@@ -31,7 +46,7 @@ def getMeanRatingForUser(userId, ratings=getRatings()):
     amount += 1
   return total / amount
 
-def getUserItemRecommendation(userId, movieId, ratings=getRatings()):
+def getUserItemRecommendation(ratings, movieId, userId):
   alpha = 0.3
   beta = 0.3
   gamma = 0.3
@@ -39,9 +54,9 @@ def getUserItemRecommendation(userId, movieId, ratings=getRatings()):
   betaEstimates = []
   gammaEstimates = []
   actual = []
-  meanUserRating = getMeanRatingForUser(userId, ratings)
-  meanItemRating = getMeanRatingForItem(movieId, ratings)
-  meanGlobal = getMeanGlobalRatingSlow(ratings)
+  meanUserRating = getMeanRatingForUser(ratings, movieId, userId)
+  meanItemRating = getMeanRatingForItem(ratings, movieId, userId)
+  meanGlobal = getMeanGlobalRatingSlow(ratings, movieId, userId)
   for rating in ratings:
     actual.append(rating["rating"])
     alphaEstimates.append(meanUserRating)
