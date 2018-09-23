f92305e65a8c124b28f94214c2de8bbacc1248b3
MMB2 <git@mmb2.click>
2018-09-23 09:36:20 +0200

    :page_facing_up: make graphs

--- graphics.py
+++ graphics.py
@@ -9,15 +9,26 @@ print("results are", data)
 
 objects = []
 runtimes = []
+rmse = []
+mae = []
 for algo in data:
   objects.append(algo["name"])
   runtimes.append(np.mean(algo["runtime"]))
+  rmse.append(np.mean(algo["rmses"]))
+  mae.append(np.mean(algo["maes"]))
+
+chartConfigs = [
+  {"yLabel": "Seconds", "fileName": "runtime", "name": "Runtimes in Seconds", "data": runtimes},
+  {"yLabel": "RMSE", "fileName": "rmse", "name": "Root Mean Squared Error", "data": rmse},
+  {"yLabel": "MAE", "fileName": "mae", "name": "Mean Absolute Error", "data": mae}
+]
 
 y_pos = np.arange(len(objects))
-
-plt.bar(y_pos, runtimes, align='center', alpha=0.5)
-plt.xticks(y_pos, objects)
-plt.ylabel('Used Algorithm')
-plt.title('Runtimes in seconds')
- 
-plt.show()
+for chart in chartConfigs:
+  plt.figure()
+  plt.bar(y_pos, chart["data"], align='center', alpha=0.5)
+  plt.xticks(y_pos, objects)
+  plt.xlabel("Algorithm Name")
+  plt.ylabel(chart["yLabel"])
+  plt.title(chart["name"])
+  plt.savefig(chart["fileName"])
